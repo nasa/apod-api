fc6ceaea08ae5be3e31cb25143aa99a8adffec7b
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-10-21 23:54:55 -0400

    only validate date if it's not None
    date will be None if parameter is not specified, passing None down makes _get_apod_chars use astropix.html instead of the date-formatted page (this url will default to the most recent)

--- application.py
+++ application.py
@@ -127,10 +127,12 @@ def _get_json_for_date(input_date, use_concept_tags, thumbs):
     if not input_date:
         # fall back to using today's date IF they didn't specify a date
         use_default_today_date = True
+        dt = input_date  # None
 
     # validate input date
-    dt = datetime.strptime(input_date, '%Y-%m-%d').date()
-    _validate_date(dt)
+    else:
+        dt = datetime.strptime(input_date, '%Y-%m-%d').date()
+        _validate_date(dt)
 
     # get data
     data = _apod_handler(dt, use_concept_tags, use_default_today_date, thumbs)
